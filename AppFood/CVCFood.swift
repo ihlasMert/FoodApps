ac6a9a381ff73f5b47714100416e109189e39e26
İhlas Mert <ihlas1mert@gmail.com>
2022-01-04 10:20:44 +0300

    initilazed label image

---
+++ AppFood/CVCFood.swift
@@ -0,0 +1,21 @@
+//
+//  CVCFood.swift
+//  AppFood
+//
+//  Created by ihlas on 4.01.2022.
+//
+
+import UIKit
+
+class CVCFood: UICollectionViewCell {// coccatach
+    
+    @IBOutlet var FoodImage: UIImageView!
+    @IBOutlet var FoodName: UILabel!
+    
+    
+    func setFood(food:Food){
+        FoodImage.image = UIImage(named: food.name!)
+        FoodName.text = food.name!
+    }
+    
+}
---
+++ AppFood/Food.swift
@@ -0,0 +1,21 @@
+//
+//  Food.swift
+//  AppFood
+//
+//  Created by ihlas on 4.01.2022.
+//
+
+import UIKit
+
+class Food: NSObject {
+    var name: String?
+    var des: String?
+    var image: String?
+    init( name: String, des: String, image: String){
+        self.name = name
+        self.des = des
+        self.image = image
+    }
+    
+    
+}
