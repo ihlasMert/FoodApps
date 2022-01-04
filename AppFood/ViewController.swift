7931166d0c30f55c1f2a39fd44618a042beb9fb7
İhlas Mert <ihlas1mert@gmail.com>
2022-01-04 11:05:58 +0300

    from property list

--- AppFood/ViewController.swift
+++ AppFood/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var lisftOfFoods = [Food]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func loadFoodsFromPropertyList(){
+        let path = Bundle.main.path(forResource: "Foods", ofType: "plist")! as String
+        let url = URL(fileURLWithPath: path)
+        
+        do{
+            let data  = try Data(contentsOf: url)
+            let plist = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil)
+            let dicArray = plist as![[String: String]]
+            for food in dicArray{
+                lisftOfFoods.append(Food(name: food["Name"]!, des: food["Des"]!, image: food["Food"]!))
+            }
 
+        }catch{
+            print("can not read file")
+            
+        }
+        
+    }
 }
 
